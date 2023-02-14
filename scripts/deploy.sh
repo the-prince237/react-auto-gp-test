82e0cf47628c6146485936895ffe4aca3e92025c
Durin TASONDOCK <temgoua484@gmail.com>
2023-02-14 19:46:58 +0100

    updating deploy.sh file to improve automatic deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -11,23 +11,40 @@ if git diff-index --quiet HEAD --;
         # push project
         git push -u origin HEAD
 
+        # create and switch to relai branch
+        git checkout --orphan relai
+
         # build
         yarn build
 
-        # create and checkout to gh-pages branch
-        git checkout --orphan gh-pages
+        # remove all files and folders instead of build
+        rm -r node_modules
+        rm -r public
+        rm -r src
+        rm *.*
 
-        # navigate into the build output directory, add and commit changes
-        git --work-tree build add --all
-        git --work-tree build commit -m "Deploy"
-
-        # push build 
-        git push origin HEAD:gh-pages --force
-
-        # remove build folder : dist and gh-pages branch
+        # mount all files and folders in build directory to project root
+        cd build
+        mv * ..
+        cd ..
         rm -r build
-        git checkout -f main
-        git branch -D gh-pages
+
+        # clean branch tree
+        git add .
+        git commit -m "preparing gh-pages root"
+
+        # Switch to gh-pages branchj and merge relai
+        git checkout gh-pages
+        git merge relai --force
+
+        # push built version to online branch
+        git push -u origin gh-pages --force
+
+        # delete relai branch
+        git branch -D relai
+
+        # come back to main branch
+        git checkout main
 
     else echo commiting changes ... 
         echo There are still not commited changes. Use \"git add\" and \"git commit\" or just \"yarn commit\" "for" quick commits
