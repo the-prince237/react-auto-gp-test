ba34a02ae334639fd3ca92c2a5f9071e276b379a
Durin TASONDOCK <temgoua484@gmail.com>
2023-02-15 08:40:28 +0100

    editing app container for the first time

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          App container <code>Edited</code> for the very first time.
         </p>
         <a
           className="App-link"
